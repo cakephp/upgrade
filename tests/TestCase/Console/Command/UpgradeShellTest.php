75b9b71d072586048c1a6bd818cc4d2787e96254
AD7six <andydawson76@gmail.com>
2014-02-11 15:21:53 +0000

    add a basic working test

---
+++ tests/TestCase/Console/Command/UpgradeShellTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Cake\Upgrade\Test\TestCase\Console\Command;
+
+use Cake\TestSuite\TestCase;
+use Cake\Upgrade\Console\Command\UpgradeShell;
+
+/**
+ * UpgradeShellTest
+ *
+ */
+class UpgradeShellTest extends TestCase {
+
+/**
+ * Upgrade shell instance
+ *
+ * @var mixed
+ */
+	public $sut;
+
+/**
+ * setUp
+ *
+ * Create a mock for all tests to use
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$out = $this->getMock('Cake\Console\ConsoleOutput', [], [], '', false);
+		$in = $this->getMock('Cake\Console\ConsoleInput', [], [], '', false);
+
+		$this->sut = $this->getMock(
+			'Cake\Upgrade\Console\Command\UpgradeShell',
+			['in', 'out', 'hr', 'err', '_stop'],
+			[$out, $out, $in]
+		);
+	}
+
+/**
+ * Basic test to simulate running on this repo
+ *
+ * Should return all files in the src directory of this repo
+ *
+ * @return void
+ */
+	public function testFiles() {
+		$repoSrc = ROOT . DS . 'src';
+
+		$this->sut->args = [$repoSrc];
+
+		$files = $this->sut->files();
+		foreach($files as &$file) {
+			$file = str_replace(DS, '/', substr($file, strlen($repoSrc) + 1));
+		}
+
+		$expected = [
+			'Config/app.default.php',
+			'Config/app.php',
+			'Config/bootstrap.php',
+			'Config/paths.php',
+			'Console/Command/UpgradeShell.php',
+			'Console/Command/Task/RenameCollectionsTask.php',
+			'Console/Command/Task/NamespacesTask.php',
+			'Console/Command/Task/StageTask.php',
+			'Console/Command/Task/AppUsesTask.php',
+			'Console/Command/Task/FixturesTask.php',
+			'Console/Command/Task/RenameClassesTask.php',
+			'Console/Command/Task/ChangeTrait.php',
+			'Console/Command/Task/LocationsTask.php',
+			'Console/cake.bat',
+			'Console/cake',
+			'Console/cake.php'
+		];
+
+		$this->assertSame($expected, $files, 'The files to process should be all files in the src folder');
+	}
+}
