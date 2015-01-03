d445c055d65f0d9ad7403264c5afbeacf7774419
Juan Basso <jrbasso@gmail.com>
2015-01-03 02:16:35 -0200

    Added String class rename to the list

---
+++ tests/TestCase/Shell/Task/RenameClassesTaskTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Cake\Upgrade\Test\TestCase\Shell\Task;
+
+use Cake\TestSuite\TestCase;
+
+/**
+ * RenameClassesTaskTest
+ *
+ */
+class RenameClassesTaskTest extends TestCase {
+
+/**
+ * Task instance
+ *
+ * @var mixed
+ */
+	public $sut;
+
+/**
+ * setUp
+ *
+ * Create a mock for all tests to use
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$io = $this->getMock('Cake\Console\ConsoleIo', [], [], '', false);
+
+		$this->sut = $this->getMock(
+			'Cake\Upgrade\Shell\Task\RenameClassesTask',
+			['in', 'out', 'hr', 'err', '_shouldProcess'],
+			[$io]
+		);
+		$this->sut->loadTasks();
+	}
+
+/**
+ * Testing the rename of string class
+ *
+ * @return void
+ */
+	public function testRenameStringClass() {
+		$this->sut->expects($this->any())
+			->method('_shouldProcess')
+			->will($this->returnValue(true));
+
+		$path = TESTS . 'test_files' . DS;
+		$result = $this->sut->process($path . 'RenameString.php');
+		$this->assertTrue($result);
+
+		$result = $this->sut->Stage->source($path . 'RenameString.php');
+		$expected = file_get_contents($path . 'RenameStringAfter.php');
+		$this->assertTextEquals($expected, $result);
+	}
+
+}
