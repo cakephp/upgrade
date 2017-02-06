1b034a1be8ded432d476e6b6511369d6404ac088
dereuromark <euromark@web.de>
2017-02-06 23:18:04 +0100

    Add Application.php

--- src/Shell/Task/SkeletonTask.php
+++ src/Shell/Task/SkeletonTask.php
@@ -61,6 +61,7 @@ class SkeletonTask extends BaseTask {
 			'config' . DS . 'paths.php',
 			'config' . DS . 'routes.php',
 			'tests' . DS . 'bootstrap.php',
+			'src' . DS . 'Application.php',
 			'src' . DS . 'Template' . DS . 'Error' . DS . 'error400.ctp',
 			'src' . DS . 'Template' . DS . 'Error' . DS . 'error500.ctp',
 			'src' . DS . 'Template' . DS . 'Layout' . DS . 'error.ctp',
