fb8209e451c189ea1bfa4cfe6747afb768060eab
Edgaras Janušauskas <edgaras.janusauskas@gmail.com>
2019-03-08 22:58:47 +0200

    Copy and merge files from cakephp/app.
    Remove unused web/database/email stuff from config and bootstrap.

--- config/app.php
+++ config/app.php
@@ -1,153 +1,91 @@
 <?php
 return [
-/**
- * Debug Level:
- *
- * Production Mode:
- * 0: No error messages, errors, or warnings shown. Flash messages redirect.
- *
- * Development Mode:
- * 1: Errors and warnings shown, model caches refreshed, flash messages halted.
- * 2: As in 1, but also with full debug messages and SQL output.
- *
- */
-	'debug' => 2,
-
-/**
- * Configure basic information about the application.
- *
- * - namespace - The namespace to find app classes under.
- * - encoding - The encoding used for HTML + database connections.
- * - base - The base directory the app resides in. If false this
- *   will be auto detected.
- * - dir - Name of app directory.
- */
-	'App' => [
-		'namespace' => 'Cake\Upgrade',
-		'encoding' => 'UTF-8',
-		'base' => false,
-		'dir' => 'src'
-	],
-
-/**
- * Configure the cache adapters.
- */
-	'Cache' => [
-		'default' => [
-			'engine' => 'File',
-		],
-
-	/**
-	 * Configure the cache used for general framework caching.  Path information,
-	 * object listings, and translation cache files are stored with this configuration.
-	 */
-		'_cake_core_' => [
-			'className' => 'File',
-			'prefix' => 'myapp_cake_core_',
-			'path' => CACHE . 'persistent/',
-			'serialize' => true,
-			'duration' => '+10 seconds',
-		],
-
-	/**
-	 * Configure the cache for model and datasource caches.  This cache configuration
-	 * is used to store schema descriptions, and table listings in connections.
-	 */
-		'_cake_model_' => [
-			'className' => 'File',
-			'prefix' => 'my_app_cake_model_',
-			'path' => CACHE . 'models/',
-			'serialize' => 'File',
-			'duration' => '+10 seconds',
-		],
-	],
-
-/**
- * Configure the Error and Exception handlers used by your application.
- *
- * By default errors are displayed using Debugger, when debug > 0 and logged by
- * Cake\Log\Log when debug = 0.
- *
- * In CLI environments exceptions will be printed to stderr with a backtrace.
- * In web environments an HTML page will be displayed for the exception.
- * While debug > 0, framework errors like Missing Controller will be displayed.
- * When debug = 0, framework errors will be coerced into generic HTTP errors.
- *
- * Options:
- *
- * - `errorLevel` - int - The level of errors you are interested in capturing.
- * - `trace` - boolean - Whether or not backtraces should be included in
- *   logged errors/exceptions.
- * - `log` - boolean - Whether or not you want exceptions logged.
- * - `exceptionRenderer` - string - The class responsible for rendering
- *   uncaught exceptions.  If you choose a custom class you should place
- *   the file for that class in app/Lib/Error. This class needs to implement a render method.
- * - `skipLog` - array - List of exceptions to skip for logging. Exceptions that
- *   extend one of the listed exceptions will also be skipped for logging.
- *   Example: `'skipLog' => array('Cake\Error\NotFoundException', 'Cake\Error\UnauthorizedException')`
- */
-	'Error' => [
-		'errorLevel' => E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED,
-		'exceptionRenderer' => 'Cake\Error\ExceptionRenderer',
-		'skipLog' => [],
-		'log' => true,
-		'trace' => true,
-	],
-
-/**
- * Configures logging options
- */
-	'Log' => [
-		'debug' => [
-			'className' => 'Cake\\Log\\Engine\\FileLog',
-			'file' => 'debug',
-			'levels' => ['notice', 'info', 'debug'],
-		],
-		'error' => [
-			'className' => 'Cake\\Log\\Engine\\FileLog',
-			'file' => 'error',
-			'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
-		],
-	],
-
-	'Session' => [
-		'defaults' => 'php',
-	],
+    /**
+     * Debug Level:
+     *
+     * Production Mode:
+     * false: No error messages, errors, or warnings shown.
+     *
+     * Development Mode:
+     * true: Errors and warnings shown.
+     */
+    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
 
     /**
-     * Email configuration.
+     * Configure basic information about the application.
      *
-     * By defining transports separately from delivery profiles you can easily
-     * re-use transport configuration across multiple profiles.
-     *
-     * You can specify multiple configurations for production, development and
-     * testing.
-     *
-     * Each transport needs a `className`. Valid options are as follows:
-     *
-     *  Mail   - Send using PHP mail function
-     *  Smtp   - Send using SMTP
-     *  Debug  - Do not send the email, just return the result
-     *
-     * You can add custom transports (or override existing transports) by adding the
-     * appropriate file to src/Mailer/Transport. Transports should be named
-     * 'YourTransport.php', where 'Your' is the name of the transport.
+     * - namespace - The namespace to find app classes under.
+     * - defaultLocale - The default locale for translation, formatting currencies and numbers, date and time.
+     * - encoding - The encoding used for HTML + database connections.
+     * - base - The base directory the app resides in. If false this
+     *   will be auto detected.
+     * - dir - Name of app directory.
      */
-    'EmailTransport' => [
-        'default' => [
-            'className' => 'Cake\Mailer\Transport\MailTransport',
-            /*
-             * The following keys are used in SMTP transports:
-             */
-            'host' => 'localhost',
-            'port' => 25,
-            'timeout' => 30,
-            'username' => null,
-            'password' => null,
-            'client' => null,
-            'tls' => null,
-            'url' => env('EMAIL_TRANSPORT_DEFAULT_URL', null),
-        ],
+    'App' => [
+        'namespace' => 'Cake\Upgrade',
+        'encoding' => env('APP_ENCODING', 'UTF-8'),
+        'defaultLocale' => env('APP_DEFAULT_LOCALE', 'en_US'),
+        'defaultTimezone' => env('APP_DEFAULT_TIMEZONE', 'UTC'),
+        'base' => false,
+        'dir' => 'src',
     ],
 
+    /**
+     * Configure the Error and Exception handlers used by your application.
+     *
+     * By default errors are displayed using Debugger, when debug is true and logged
+     * by Cake\Log\Log when debug is false.
+     *
+     * In CLI environments exceptions will be printed to stderr with a backtrace.
+     * In web environments an HTML page will be displayed for the exception.
+     * With debug true, framework errors like Missing Controller will be displayed.
+     * When debug is false, framework errors will be coerced into generic HTTP errors.
+     *
+     * Options:
+     *
+     * - `errorLevel` - int - The level of errors you are interested in capturing.
+     * - `trace` - boolean - Whether or not backtraces should be included in
+     *   logged errors/exceptions.
+     * - `log` - boolean - Whether or not you want exceptions logged.
+     * - `exceptionRenderer` - string - The class responsible for rendering
+     *   uncaught exceptions. If you choose a custom class you should place
+     *   the file for that class in src/Error. This class needs to implement a
+     *   render method.
+     * - `skipLog` - array - List of exceptions to skip for logging. Exceptions that
+     *   extend one of the listed exceptions will also be skipped for logging.
+     *   E.g.:
+     *   `'skipLog' => ['Cake\Http\Exception\NotFoundException', 'Cake\Http\Exception\UnauthorizedException']`
+     * - `extraFatalErrorMemory` - int - The number of megabytes to increase
+     *   the memory limit by when a fatal error is encountered. This allows
+     *   breathing room to complete logging or error handling.
+     */
+    'Error' => [
+        'errorLevel' => E_ALL,
+        'exceptionRenderer' => 'Cake\Error\ExceptionRenderer',
+        'skipLog' => [],
+        'log' => true,
+        'trace' => true,
+    ],
+
+    /**
+     * Configures logging options
+     */
+    'Log' => [
+        'debug' => [
+            'className' => 'Cake\Log\Engine\FileLog',
+            'path' => LOGS,
+            'file' => 'debug',
+            'url' => env('LOG_DEBUG_URL', null),
+            'scopes' => false,
+            'levels' => ['notice', 'info', 'debug'],
+        ],
+        'error' => [
+            'className' => 'Cake\Log\Engine\FileLog',
+            'path' => LOGS,
+            'file' => 'error',
+            'url' => env('LOG_ERROR_URL', null),
+            'scopes' => false,
+            'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        ],
+    ],
 ];
--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,42 +1,24 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 0.10.8.2117
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.8
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-namespace App\Config;
 
-/**
- * Configure paths required to find CakePHP + general filepath
- * constants
+/*
+ * Configure paths required to find CakePHP + general filepath constants
  */
 require __DIR__ . '/paths.php';
 
-// Use composer to load the autoloader.
-if (file_exists(ROOT . '/vendor/autoload.php')) {
-	require ROOT . '/vendor/autoload.php';
-}
-
-// If composer is not used, use CakePHP's classloader to autoload the framework
-// and the application. You will also need setup autoloading for plugins by
-// passing `autoload' => true for `Plugin::loadAll()` or `Plugin::load()`
-if (!class_exists('Cake\Core\Configure')) {
-	require CAKE . 'Core/ClassLoader.php';
-	$loader = new \Cake\Core\ClassLoader;
-	$loader->register();
-	$loader->addNamespace('Cake', CAKE);
-	$loader->addNamespace(\Cake\Core\Configure::read('App.namespace'), APP);
-}
-
-/**
+/*
  * Bootstrap CakePHP.
  *
  * Does the various bits of setup that CakePHP needs to do.
@@ -49,15 +31,10 @@ require CORE_PATH . 'config' . DS . 'bootstrap.php';
 
 use Cake\Cache\Cache;
 use Cake\Console\ConsoleErrorHandler;
-use Cake\Core\App;
 use Cake\Core\Configure;
 use Cake\Core\Configure\Engine\PhpConfig;
-use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
 use Cake\Error\ErrorHandler;
 use Cake\Log\Log;
-use Cake\Network\Email\Email;
-use Cake\Utility\Inflector;
 
 /**
  * Read configuration file and inject configuration into various
@@ -68,77 +45,44 @@ use Cake\Utility\Inflector;
  * that changes from configuration that does not. This makes deployment simpler.
  */
 try {
-	Configure::config('default', new PhpConfig());
-	Configure::load('app', 'default', false);
-
-	// Load an environment local configuration file.
-	// You can use this file to provide local overrides to your
-	// shared configuration.
-	// Configure::load('app.local', 'default');
+    Configure::config('default', new PhpConfig());
+    Configure::load('app', 'default', false);
 } catch (\Exception $e) {
-	die('Unable to load config/app.php. Create it by copying config/app.default.php to config/app.php.');
+    exit($e->getMessage() . "\n");
 }
 
-/**
- * Uncomment this line and correct your server timezone to fix
- * any date & time related errors.
+/*
+ * Load an environment local configuration file.
+ * You can use a file like app_local.php to provide local overrides to your
+ * shared configuration.
  */
-	//date_default_timezone_set('UTC');
+//Configure::load('app_local', 'default');
 
-/**
+/*
+ * Set the default server timezone. Using UTC makes time calculations / conversions easier.
+ * Check http://php.net/manual/en/timezones.php for list of valid timezone strings.
+ */
+date_default_timezone_set(Configure::read('App.defaultTimezone'));
+
+/*
  * Configure the mbstring extension to use the correct encoding.
  */
 mb_internal_encoding(Configure::read('App.encoding'));
 
-/**
+/*
+ * Set the default locale. This controls how dates, number and currency is
+ * formatted and sets the default language to use for translations.
+ */
+ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
+
+/*
  * Register application error and exception handlers.
  */
-if (php_sapi_name() === 'cli') {
-	(new ConsoleErrorHandler(Configure::consume('Error')))->register();
+$isCli = PHP_SAPI === 'cli';
+if ($isCli) {
+    (new ConsoleErrorHandler(Configure::read('Error')))->register();
 } else {
-	(new ErrorHandler(Configure::consume('Error')))->register();
+    (new ErrorHandler(Configure::read('Error')))->register();
 }
 
-/**
- * Set the full base url.
- * This URL is used as the base of all absolute links.
- *
- * If you define fullBaseUrl in your config file you can remove this.
- */
-if (!Configure::read('App.fullBaseUrl')) {
-	$s = null;
-	if (env('HTTPS')) {
-		$s = 's';
-	}
-
-	$httpHost = env('HTTP_HOST');
-	if (isset($httpHost)) {
-		Configure::write('App.fullBaseUrl', 'http' . $s . '://' . $httpHost);
-	}
-	unset($httpHost, $s);
-}
-
-Cache::config(Configure::consume('Cache'));
-ConnectionManager::config(Configure::consume('Datasources'));
-Email::configTransport(Configure::consume('EmailTransport'));
-Email::config(Configure::consume('Email'));
-Log::config(Configure::consume('Log'));
-
-/**
- * Custom Inflector rules, can be set to correctly pluralize or singularize table, model, controller names or whatever other
- * string is passed to the inflection functions
- *
- * Inflector::rules('singular', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
- * Inflector::rules('plural', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
- *
- */
-
-/**
- * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
- * Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
- * advanced ways of loading plugins
- *
- * Plugin::loadAll(); // Loads all plugins at once
- * Plugin::load('DebugKit'); //Loads a single plugin named DebugKit
- *
- */
+Log::setConfig(Configure::consume('Log'));
