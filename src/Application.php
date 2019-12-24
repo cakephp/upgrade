dcb3677111f457cfeb9f82518dcccabb6d5d7347
Corey Taylor <corey.taylor.fl@gmail.com>
2019-12-24 00:40:37 -0600

    Disabled and documented phpcs rules until dependencies are fixed. Restored php headers.

--- src/Application.php
+++ src/Application.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
@@ -13,9 +14,6 @@
  * @since     3.3.0
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
-
-declare(strict_types=1);
-
 namespace Cake\Upgrade;
 
 use Cake\Console\CommandCollection;
