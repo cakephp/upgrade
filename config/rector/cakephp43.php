4ba7b06483306e32411610f7762ac5187dbf1ad3
Mark Story <mark@mark-story.com>
2021-11-01 22:31:34 -0400

    Add missing config set
    I forgot to add this config file when adding the 4.3 rules to rector.
    
    Fixes cakephp/docs#7231

---
+++ config/rector/cakephp43.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Rector\CakePHP\Set\CakePHPSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(CakePHPSetList::CAKEPHP_43);
+};
