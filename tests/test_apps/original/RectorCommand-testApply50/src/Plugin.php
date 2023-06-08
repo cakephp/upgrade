d7faa4b99053e923c39e4b78eb354058be14c1db
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-06-08 14:25:42 +0200

    add plugin property rule

---
+++ tests/test_apps/original/RectorCommand-testApply50/src/Plugin.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace MyPlugin;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for AlfredCovid
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * Do bootstrapping or not
+     *
+     * @var bool
+     */
+    protected $bootstrapEnabled = true;
+
+    /**
+     * Console middleware
+     *
+     * @var bool
+     */
+    protected $consoleEnabled = true;
+
+    /**
+     * Enable middleware
+     *
+     * @var bool
+     */
+    protected $middlewareEnabled = true;
+
+    /**
+     * Register container services
+     *
+     * @var bool
+     */
+    protected $servicesEnabled = true;
+
+    /**
+     * Load routes or not
+     *
+     * @var bool
+     */
+    protected $routesEnabled = true;
+
+    /**
+     * The path to this plugin.
+     *
+     * @var string|null
+     */
+    protected $path = null;
+
+    /**
+     * The class path for this plugin.
+     *
+     * @var string|null
+     */
+    protected $classPath = null;
+
+    /**
+     * The config path for this plugin.
+     *
+     * @var string|null
+     */
+    protected $configPath = null;
+
+    /**
+     * The templates path for this plugin.
+     *
+     * @var string|null
+     */
+    protected $templatePath = null;
+
+    /**
+     * The name of this plugin
+     *
+     * @var string|null
+     */
+    protected $name = null;
+}
---
+++ tests/test_apps/upgraded/RectorCommand-testApply50/src/Plugin.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace MyPlugin;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for AlfredCovid
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * Do bootstrapping or not
+     *
+     * @var bool
+     */
+    protected bool $bootstrapEnabled = true;
+
+    /**
+     * Console middleware
+     *
+     * @var bool
+     */
+    protected bool $consoleEnabled = true;
+
+    /**
+     * Enable middleware
+     *
+     * @var bool
+     */
+    protected bool $middlewareEnabled = true;
+
+    /**
+     * Register container services
+     *
+     * @var bool
+     */
+    protected bool $servicesEnabled = true;
+
+    /**
+     * Load routes or not
+     *
+     * @var bool
+     */
+    protected bool $routesEnabled = true;
+
+    /**
+     * The path to this plugin.
+     *
+     * @var string|null
+     */
+    protected ?string $path = null;
+
+    /**
+     * The class path for this plugin.
+     *
+     * @var string|null
+     */
+    protected ?string $classPath = null;
+
+    /**
+     * The config path for this plugin.
+     *
+     * @var string|null
+     */
+    protected ?string $configPath = null;
+
+    /**
+     * The templates path for this plugin.
+     *
+     * @var string|null
+     */
+    protected ?string $templatePath = null;
+
+    /**
+     * The name of this plugin
+     *
+     * @var string|null
+     */
+    protected ?string $name = null;
+}
