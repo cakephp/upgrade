01503c12c97548b80da9c3fd7d2f56bd916c4766
Mark Scherer <dereuromark@users.noreply.github.com>
2023-09-21 16:49:54 +0200

    Fix up deprecations

--- config/app.php
+++ config/app.php
@@ -79,7 +79,7 @@ return [
             'path' => LOGS,
             'file' => 'debug',
             'url' => env('LOG_DEBUG_URL', null),
-            'scopes' => false,
+            'scopes' => null,
             'levels' => ['notice', 'info', 'debug'],
         ],
         'error' => [
@@ -87,7 +87,7 @@ return [
             'path' => LOGS,
             'file' => 'error',
             'url' => env('LOG_ERROR_URL', null),
-            'scopes' => false,
+            'scopes' => null,
             'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
         ],
     ],
