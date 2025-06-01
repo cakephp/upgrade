280253fabf6e71bf1b072a60bc23e22bc8e9fb9a
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2025-06-01 12:46:31 +0200

    add custom rector rule to 5.2 config + add test

--- tests/test_apps/upgraded/RectorCommand-testApply52/src/SomeTest.php
+++ tests/test_apps/upgraded/RectorCommand-testApply52/src/SomeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace MyPlugin;
 
 use Cake\Console\Arguments;
+use Cake\ORM\Entity;
 
 class SomeTest extends TestCase
 {
@@ -11,5 +12,11 @@ class SomeTest extends TestCase
     {
         $args = new Arguments([], ['a' => [1, 2]], []);
         $option = $args->getArrayOption('a');
+
+        $entity = new Entity();
+        // This should be changed to patch
+        $entity->patch(['paging' => 'test']);
+        // This should not be changed
+        $entity->set('paging', 'test');
     }
 }
