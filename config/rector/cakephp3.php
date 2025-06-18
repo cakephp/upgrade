a13b9dc07b494748619b928d58997cd5ef40f382
mscherer <dereuromark@web.de>
2025-06-18 09:42:37 +0200

    Have one cake3 set.

---
+++ config/rector/cakephp3.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Cake\Upgrade\Rector\Set\CakePHPSetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([CakePHPSetList::CAKEPHP_30, CakePHPSetList::CAKEPHP_34, CakePHPSetList::CAKEPHP_35, CakePHPSetList::CAKEPHP_36, CakePHPSetList::CAKEPHP_37, CakePHPSetList::CAKEPHP_38]);
+};
