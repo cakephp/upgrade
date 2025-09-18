7d5724667fd9e24f7408bda0d0e18dcfff23fe95
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2025-09-18 09:55:59 +0200

    add tests

--- tests/test_apps/upgraded/RectorCommand-testApply53/src/SomeTest.php
+++ tests/test_apps/upgraded/RectorCommand-testApply53/src/SomeTest.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 namespace MyPlugin;
 
 use Cake\ORM\Entity;
+use Cake\ORM\Locator\LocatorAwareTrait;
 
 class SomeTest
 {
+    use LocatorAwareTrait;
+
     public function testRenames(): void
     {
         $entity = new Entity();
         $result = !$entity->hasValue('test');
+
+        $table = $this->fetchTable('Articles');
+        $expr = $table->find()->expr();
     }
 }
