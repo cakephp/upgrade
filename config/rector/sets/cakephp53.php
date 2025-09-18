b93a879c46e0fa614377dd9ab1e67de67b330523
ADmad <admad.coder@gmail.com>
2025-09-18 13:03:13 +0530

    Rename Query::newExpr() to Query::expr()
    Refs cakephp/cakephp#18916

--- config/rector/sets/cakephp53.php
+++ config/rector/sets/cakephp53.php
@@ -3,8 +3,13 @@ declare(strict_types=1);
 
 use Cake\Upgrade\Rector\Rector\MethodCall\EntityIsEmptyRector;
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
 
 # @see https://book.cakephp.org/5/en/appendices/5-3-migration-guide.html
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->ruleWithConfiguration(RenameMethodRector::class, [
+        new MethodCallRename('Cake\Database\Query', 'newExpr', 'expr'),
+    ]);
     $rectorConfig->rule(EntityIsEmptyRector::class);
 };
