434375472eddd27f05fe0557e612ecc3b39ee9a3
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2025-11-27 11:29:00 +0100

    cleanup migrations 5.0 config

--- config/rector/sets/migrations50.php
+++ config/rector/sets/migrations50.php
@@ -9,42 +9,49 @@ use Rector\Renaming\ValueObject\RenameClassConstFetch;
  * @see https://github.com/cakephp/migrations/releases/5.0.0/
  */
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [
+
+    $constantMap = [
         // Standard types
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_STRING', 'TYPE_STRING'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_CHAR', 'TYPE_CHAR'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_TEXT', 'TYPE_TEXT'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_INTEGER', 'TYPE_INTEGER'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_TINY_INTEGER', 'TYPE_TINYINTEGER'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_SMALL_INTEGER', 'TYPE_SMALLINTEGER'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_BIG_INTEGER', 'TYPE_BIGINTEGER'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_FLOAT', 'TYPE_FLOAT'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_DECIMAL', 'TYPE_DECIMAL'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_DATETIME', 'TYPE_DATETIME'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_TIMESTAMP', 'TYPE_TIMESTAMP'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_TIME', 'TYPE_TIME'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_DATE', 'TYPE_DATE'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_BINARY', 'TYPE_BINARY'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_BINARYUUID', 'TYPE_BINARY_UUID'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_BOOLEAN', 'TYPE_BOOLEAN'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_JSON', 'TYPE_JSON'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_UUID', 'TYPE_UUID'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_NATIVEUUID', 'TYPE_NATIVE_UUID'),
+        'PHINX_TYPE_STRING' => 'TYPE_STRING',
+        'PHINX_TYPE_CHAR' => 'TYPE_CHAR',
+        'PHINX_TYPE_TEXT' => 'TYPE_TEXT',
+        'PHINX_TYPE_INTEGER' => 'TYPE_INTEGER',
+        'PHINX_TYPE_TINY_INTEGER' => 'TYPE_TINYINTEGER',
+        'PHINX_TYPE_SMALL_INTEGER' => 'TYPE_SMALLINTEGER',
+        'PHINX_TYPE_BIG_INTEGER' => 'TYPE_BIGINTEGER',
+        'PHINX_TYPE_FLOAT' => 'TYPE_FLOAT',
+        'PHINX_TYPE_DECIMAL' => 'TYPE_DECIMAL',
+        'PHINX_TYPE_DATETIME' => 'TYPE_DATETIME',
+        'PHINX_TYPE_TIMESTAMP' => 'TYPE_TIMESTAMP',
+        'PHINX_TYPE_TIME' => 'TYPE_TIME',
+        'PHINX_TYPE_DATE' => 'TYPE_DATE',
+        'PHINX_TYPE_BINARY' => 'TYPE_BINARY',
+        'PHINX_TYPE_BINARYUUID' => 'TYPE_BINARY_UUID',
+        'PHINX_TYPE_BOOLEAN' => 'TYPE_BOOLEAN',
+        'PHINX_TYPE_JSON' => 'TYPE_JSON',
+        'PHINX_TYPE_UUID' => 'TYPE_UUID',
+        'PHINX_TYPE_NATIVEUUID' => 'TYPE_NATIVE_UUID',
 
         // Geospatial types
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_GEOMETRY', 'TYPE_GEOMETRY'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_POINT', 'TYPE_POINT'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_LINESTRING', 'TYPE_LINESTRING'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_POLYGON', 'TYPE_POLYGON'),
+        'PHINX_TYPE_GEOMETRY' => 'TYPE_GEOMETRY',
+        'PHINX_TYPE_POINT' => 'TYPE_POINT',
+        'PHINX_TYPE_LINESTRING' => 'TYPE_LINESTRING',
+        'PHINX_TYPE_POLYGON' => 'TYPE_POLYGON',
 
         // Geospatial array constant
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPES_GEOSPATIAL', 'TYPES_GEOSPATIAL'),
+        'PHINX_TYPES_GEOSPATIAL' => 'TYPES_GEOSPATIAL',
 
         // Database-specific types
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_YEAR', 'TYPE_YEAR'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_CIDR', 'TYPE_CIDR'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_INET', 'TYPE_INET'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_MACADDR', 'TYPE_MACADDR'),
-        new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', 'PHINX_TYPE_INTERVAL', 'TYPE_INTERVAL'),
-    ]);
+        'PHINX_TYPE_YEAR' => 'TYPE_YEAR',
+        'PHINX_TYPE_CIDR' => 'TYPE_CIDR',
+        'PHINX_TYPE_INET' => 'TYPE_INET',
+        'PHINX_TYPE_MACADDR' => 'TYPE_MACADDR',
+        'PHINX_TYPE_INTERVAL', 'TYPE_INTERVAL',
+    ];
+
+    foreach ($constantMap as $oldConstant => $newConstant) {
+        $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [
+            new RenameClassConstFetch('Migrations\Db\Adapter\AdapterInterface', $oldConstant, $newConstant),
+        ]);
+    }
 };
