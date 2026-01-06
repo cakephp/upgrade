f7bdbf716784369697f34375e71382bb4dc0222a
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2026-01-06 13:57:55 +0100

    fix tests

--- tests/TestCase/Rector/MethodCall/StaticConnectionHelperRector/config/configured_rule.php
+++ tests/TestCase/Rector/MethodCall/StaticConnectionHelperRector/config/configured_rule.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Cake\Upgrade\Rector\Rector\MethodCall\StaticConnectionHelperRector;
+use Cake\Upgrade\Rector\Cake5\StaticConnectionHelperRector;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
