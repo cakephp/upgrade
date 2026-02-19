280002f7b4c51dff35e8efc8df7f434f77919839
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-02-19 17:42:10 +0700

    Fix use of deprecated Expr\ArrayItem

--- src/Rector/Rector/MethodCall/BreadcrumbsHelperAddManyRector.php
+++ src/Rector/Rector/MethodCall/BreadcrumbsHelperAddManyRector.php
@@ -5,7 +5,7 @@ namespace Cake\Upgrade\Rector\Rector\MethodCall;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\ArrayItem;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Scalar\String_;
