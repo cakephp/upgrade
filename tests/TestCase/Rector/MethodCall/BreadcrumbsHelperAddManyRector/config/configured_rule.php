ca61a8c288378dca34fe7dddd36a673d06f32696
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2026-04-04 10:24:47 +0200

    merge 5.x => 6.x + Makefile adjustments

--- tests/TestCase/Rector/MethodCall/BreadcrumbsHelperAddManyRector/config/configured_rule.php
+++ tests/TestCase/Rector/MethodCall/BreadcrumbsHelperAddManyRector/config/configured_rule.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use Cake\Upgrade\Rector\Rector\MethodCall\BreadcrumbsHelperAddManyRector;
+use Cake\Upgrade\Rector\Cake5\BreadcrumbsHelperAddManyRector;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
