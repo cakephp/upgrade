8ca223fb91d4bdb9f6eef215ced44386870aa1ad
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-24 22:48:01 +0700

    move source different namespace to stubs

--- structarmed.php
+++ structarmed.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Boundwize\StructArmed\Architecture;
 use Boundwize\StructArmed\Preset\Preset;
-use Boundwize\StructArmed\Preset\Presets\Psr4Preset;
 
 return Architecture::define()
     ->withPreset(Preset::PSR4());
