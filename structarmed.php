52834b806040f71c11e2d2c20beb4470234a3fbe
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-24 22:45:38 +0700

    move no namespace on purpose to stubs with classmap

--- structarmed.php
+++ structarmed.php
@@ -7,9 +7,4 @@ use Boundwize\StructArmed\Preset\Preset;
 use Boundwize\StructArmed\Preset\Presets\Psr4Preset;
 
 return Architecture::define()
-    ->skip([
-        Psr4Preset::CLASSES_MUST_MATCH_COMPOSER => [
-            __DIR__ . '/tests/TestCase/Rector/Namespace_/AppUsesStaticCallToUseStatementRector/Source',
-        ],
-    ])
     ->withPreset(Preset::PSR4());
