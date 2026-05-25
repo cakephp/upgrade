7dd698e664a85699faa62ce043fc685676defe0e
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2026-05-25 14:07:55 +0200

    fix wrong namespace definitions

--- tests/TestCase/Rector/MethodCall/RemoveMethodCallArgsRector/Source/SomeModelType.php
+++ tests/TestCase/Rector/MethodCall/RemoveMethodCallArgsRector/Source/SomeModelType.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Cake\Upgrade\Test\TestCase\Rector\MethodCall\AddMethodCallArgsRector\Source;
+namespace Cake\Upgrade\Test\TestCase\Rector\MethodCall\RemoveMethodCallArgsRector\Source;
 
 final class SomeModelType
 {
--- tests/TestCase/Rector/MethodCall/RemoveMethodCallArgsRector/config/configured_rule.php
+++ tests/TestCase/Rector/MethodCall/RemoveMethodCallArgsRector/config/configured_rule.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Cake\Upgrade\Rector\Rector\MethodCall\RemoveMethodCallRector;
 use Cake\Upgrade\Rector\ValueObject\RemoveMethodCall;
-use Cake\Upgrade\Test\TestCase\Rector\MethodCall\AddMethodCallArgsRector\Source\SomeModelType;
+use Cake\Upgrade\Test\TestCase\Rector\MethodCall\RemoveMethodCallArgsRector\Source\SomeModelType;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
